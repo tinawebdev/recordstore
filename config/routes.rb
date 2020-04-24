ea1a3dbbe08b5da3b182ff5d0dca0f1526aa3c67
Kristina <codingkris@yahoo.com>
2020-04-24 15:44:02 +0300

    Routes and controllers updated

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,9 @@ Rails.application.routes.draw do
   end
 
   root to: "home#index"
+
+  post "refresh", controller: :refresh, action: :create
+  post "signin", controller: :signin, action: :create
+  post "signin", controller: :signup, action: :create
+  delete "signin", controller: :signin, action: :destroy
 end
